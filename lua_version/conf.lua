73f8725b76a140edd8ff80da9aef89cfcf75885f
mint <apribot@gmail.com>
2018-10-07 23:32:19 -0400

    nice nice

---
+++ lua_version/conf.lua
@@ -0,0 +1,5 @@
+-- forces this thing to run headless because a black box is no use to anyone
+-- at this point
+function love.conf(t)
+    t.window = false
+end
--- lua_version/main.lua
+++ lua_version/main.lua
@@ -1,29 +1,49 @@
-
-local samples = 19999
+local samples = 1000 * 265 --19999
 local rate = 44100
 local bits = 8
 local channels = 1
 
-soundData = love.sound.newSoundData( samples, rate, bits, channels )
-
-
-print('creating samples')
-
-for t=1,samples-1 do
-	soundData:setSample(t, ((((  (t/3) * (t/2)  )%255)/127.5)-1))
+function waveFunction(t)
+	return t
 end
-	
-print('done')
 
-sample = love.audio.newSource(soundData)
+function generateSample()
+	soundData = love.sound.newSoundData( samples, rate, bits, channels )
 
---src1:setVolume(0.9) -- 90% of ordinary volume
---src2:setVolume(0.7)
+	for t=1,samples-1 do
+		position = ((((  waveFunction(t)  )%255)/127.5)-1)
+		-- intermix -1 oct tone because fatness and because i wanna
+		position = (position * 0.8) + (((((  waveFunction(t)  )%510)/255)-1) * 0.2)
+		soundData:setSample(t, position)
+	end
 
-sample:play()
-sample:rewind()
-sample:setPitch(0.5) -- one octave lower
-sample:play()
-sample:rewind()
-sample:setPitch(0.5) -- one octave lower
-sample:play()
+	sample = love.audio.newSource(soundData)
+	sample:setLooping(true)
+
+	return sample
+end
+
+function playNote(sound, noteOffset)
+  love.audio.stop(sound)
+  pitchMod = 2^(noteOffset / 12.0)
+  sound:setPitch(pitchMod)
+  sound:play()
+end
+
+function stopAll(sound)	
+	love.audio.stop(sound)
+end
+
+sample = generateSample()
+
+
+print('playing 1')
+playNote(sample, 24)
+love.timer.sleep( 1 )
+print('playing 2')
+playNote(sample, 0)
+love.timer.sleep( 1 )
+stopAll(sample)
+
+-- forces this thing to terminate like a normal lua script
+love.event.quit(0)
